0e72636c8a754c345c182ef3583c9173b7889357
Dishant <101065566+Phantomgaze@users.noreply.github.com>
2024-10-28 22:08:06 +0530

    first code deploy

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your configuration options here
+  // For example:
+  // reactStrictMode: true,
+  // images: {
+  //   domains: ['example.com'],
+  // },
+}
+
+module.exports = nextConfig
